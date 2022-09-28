2d09b05b2b8c0b9ad2b27af0639dd10fd3381a84
QueloNovax <goyolemos@hotmail.com>
2022-09-27 23:08:12 -0300

    Pude hacer que se puedan seleccionar, pero aún falta la lógica para hacer que se deseleccionen las demás opciones de la pregunta

--- main.js
+++ main.js
@@ -1,62 +1,74 @@
 // MENU ANIMATIONS
 const hamburger = document.querySelector(".hamburger");
-const navMenu=document.querySelector(".menu");
+const navMenu = document.querySelector(".menu");
 
-hamburger.addEventListener("click", ()=>{
+hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 })
 
-document.querySelectorAll('.menu-link').forEach(n => n.addEventListener('click',()=>{
+document.querySelectorAll('.menu-link').forEach(n => n.addEventListener('click', () => {
     hamburger.classList.remove('active');
     navMenu.classList.remove('active');
 }));
 
 //SEARCHBAR ANIMATIONS
 
-const searchInput=document.querySelector('.searchbar-input');
-const searchButton=document.querySelector('.searchbar-box');
-const searchCancel=document.querySelector('.searchbar-cancel-box');
-const logoContainer=document.querySelector('.logo-container');
-const searchbarContainer=document.querySelector('.menu-search-container');
+const searchInput = document.querySelector('.searchbar-input');
+const searchButton = document.querySelector('.searchbar-box');
+const searchCancel = document.querySelector('.searchbar-cancel-box');
+const logoContainer = document.querySelector('.logo-container');
+const searchbarContainer = document.querySelector('.menu-search-container');
 
-searchButton.addEventListener('click', ()=>{
-  searchInput.classList.toggle('active');
-  searchButton.classList.toggle('active');
-  searchCancel.classList.toggle('active');
-  logoContainer.classList.toggle('active');  
-  searchbarContainer.classList.toggle('active');  
+searchButton.addEventListener('click', () => {
+    searchInput.classList.toggle('active');
+    searchButton.classList.toggle('active');
+    searchCancel.classList.toggle('active');
+    logoContainer.classList.toggle('active');
+    searchbarContainer.classList.toggle('active');
 })
 
-searchCancel.addEventListener('click', ()=>{
+searchCancel.addEventListener('click', () => {
     searchInput.classList.remove('active');
     searchButton.classList.remove('active');
     searchCancel.classList.remove('active');
     logoContainer.classList.remove('active');
     searchbarContainer.classList.remove('active');
 
-    searchInput.querySelector('input').value='';
+    searchInput.querySelector('input').value = '';
 })
 
-const logoButton=document.querySelector('.logo-container img');
+const logoButton = document.querySelector('.logo-container img');
 
-logoButton.addEventListener('click',()=>{
-    window.location='./index.html';
+logoButton.addEventListener('click', () => {
+    window.location = './index.html';
 })
 
 //QUESTIONNAIRE OPTION SELECTION
 
-// const qaireOption = document.querySelectorAll(".opcion").forEach(n =>n.addEventListener('click',()=>{
-//     qaireOption[n].classList.toggle('active');
-//     }));
+const qaireOption = document.querySelectorAll(".opcion")
+const qaireOption1 = document.querySelectorAll(".pregunta1")
+const qaireOption2 = document.querySelectorAll(".pregunta2")
+const qaireOption3 = document.querySelectorAll(".pregunta3")
+const qaireOption4 = document.querySelectorAll(".pregunta4")
+const qaireOption5 = document.querySelectorAll(".pregunta5")
 
-    // function activarOpcion(){
-    //     qaireOption.classList.toggle('active');
-    // }
-    
-    // qaireOption.onclick = function (){
-    //     activarOpcion();
-    // };
-// document.querySelectorAll('.menu-link').forEach(n => n.addEventListener('click',()=>{
-//     qaireOption.classList.remove('active');
-// }));
+qaireOption1.forEach(n => n.addEventListener('click', () => {
+    n.classList.toggle('active');
+}));
+
+qaireOption2.forEach(n => n.addEventListener('click', () => {
+    n.classList.toggle('active');
+}));
+
+qaireOption3.forEach(n => n.addEventListener('click', () => {
+    n.classList.toggle('active');
+}));
+
+qaireOption4.forEach(n => n.addEventListener('click', () => {
+    n.classList.toggle('active');
+}));
+
+qaireOption5.forEach(n => n.addEventListener('click', () => {
+    n.classList.toggle('active');
+}));
